101efbd8b02dc0c0cd24f0975ef6a49efc5e53a5
joãoA <joaovictor.dudu03@gmail.com>
2025-03-06 00:56:08 -0300

    configurando o projeto  e estruturando o layout

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const imagemin = require('gulp-imagemin')
+const sourcemaps = require('gulp-sourcemaps')
+
+function styles(){
+    return gulp.src('./src/styles/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+        outputStyle: 'compressed'
+    }))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('./dist/styles'))
+}
+
+function images(){
+    return gulp.src('./src/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'))
+}
+
+
+exports.default = gulp.parallel(styles,images)
+exports.watch = (function(){
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+})
